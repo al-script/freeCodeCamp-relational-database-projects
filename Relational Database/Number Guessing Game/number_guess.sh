3df600603d3e3fc3b87aa39312301a52f3b3c4f4
al-script <105756627+al-script@users.noreply.github.com>
2022-09-10 13:35:05 -0700

    Update number_guess.sh

--- Relational Database/Number Guessing Game/number_guess.sh
+++ Relational Database/Number Guessing Game/number_guess.sh
@@ -2,21 +2,26 @@
 
 PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
 
-#gotta update the database where necessary
+######
+#gotta make the database users, with columns: username VARCHAR(23), games_played INT, best_game INT
 #gotta handle where input is anything but an integer
+#gotta properly generate the random number
+#gotta figure out why it's not prompting the username
+#indent
+
+######
 #should games played be updated once begin the game, or once end the game?
 #should user creation be handled at start of game or end of game?
 
 HANDLE_UPDATING_DATABASE_ENDGAME(){
 if [[ $IS_USER -eq 1 ]]; then
-  UPDATE_GAMES_PLAYED=$($PSQL "")
+  UPDATE_GAMES_PLAYED=$($PSQL "UPDATE users SET games_played=$((GAMES_PLAYED++)) WHERE username='$USERNAME'")
   if [[ $NUMBER_OF_GUESSES -lt $BEST_GAME ]]
   then
-  #set best game to number_of guesses
-  UPDATE_BEST_GUESS=$($PSQL "")
+  UPDATE_BEST_GUESS=$($PSQL "UPDATE users SET best_game=$NUMBER_OF_GUESSES WHERE username='$USERNAME'")
   fi
 elif [[ $IS_USER -eq 0 ]]; then
-  INSERT_NEW_USER=$($PSQL "INSERT INTO users(username, total_games, best_game) VALUES($USERNAME, 1, $NUMBER_OF_GUESSES)")
+  INSERT_NEW_USER=$($PSQL "INSERT INTO users(username, games_played, best_game) VALUES($USERNAME, 1, $NUMBER_OF_GUESSES)")
 fi
 }
 
@@ -26,19 +31,19 @@ if [[ $USER_GUESS -eq $SECRET_NUMBER ]]; then
   HANDLE_UPDATING_DATABASE_ENDGAME
 elif [[ $USER_GUESS -lt $SECRET_NUMBER ]]; then
   echo -e "\nIt's higher than that, guess again:"
-  NUMBER_OF_GUESSES++
+  ((NUMBER_OF_GUESSES++))
   read USER_GUESS
   GUESSING_GAME_CONT
 elif [[ $USER_GUESS -gt $SECRET_NUMBER ]]; then
   echo -e "\nIt's lower than that, guess again:"
-  NUMBER_OF_GUESSES++
+  ((NUMBER_OF_GUESSES++))
   read USER_GUESS
   GUESSING_GAME_CONT
 fi
 }
 
 GUESSING_GAME_INIT() {
-SECRET_NUMBER=
+SECRET_NUMBER=((1 + $RANDOM % 1000))
 echo -e "\nGuess the secret number between 1 and 1000:"
 read USER_GUESS
 NUMBER_OF_GUESSES=1
@@ -46,16 +51,12 @@ GUESSING_GAME_CONT
 fi
 }
 
-INIT() {
-echo -e "\nEnter your username:"
-read USERNAME
-USERNAME_FORMATTED=
-IS_USER=$($PSQL "")
+USER_GREETING() {
 if [[ $IS_USER -eq 1 ]]; then
-GAMES_PLAYED=$($PSQL "")
-GAMES_PLAYED_FORMATTED=
-BEST_GAME=$($PSQL "")
-BEST_GAME_FORMATTED=
+GAMES_PLAYED=$($PSQL "SELECT games_played FROM users WHERE username='$USERNAME'")
+GAMES_PLAYED_FORMATTED=$(echo $GAMES_PLAYED | sed 's/ //')
+BEST_GAME=$($PSQL "SELECT best_game FROM users WHERE username='$USERNAME'")
+BEST_GAME_FORMATTED=$(echo $BEST_GAME | sed 's/ //')
 echo -e "\nWelcome back, $USERNAME_FORMATTED! You have played $GAMES_PLAYED_FORMATTED games, and your best game took $BEST_GAME_FORMATTED guesses."
 GUESSING_GAME_INIT
 elif [[ $IS_USER -eq 0 ]]; then
@@ -64,4 +65,12 @@ GUESSING_GAME_INIT
 fi
 }
 
-INIT
+ENTER_USERNAME() {
+echo -e "\nEnter your username:"
+read USERNAME
+USERNAME_FORMATTED=$(echo $USERNAME | sed 's/ //')
+IS_USER=$($PSQL "SELECT COUNT(*) FROM users WHERE username='$USERNAME'")
+USER_GREETING
+}
+
+ENTER_USERNAME
