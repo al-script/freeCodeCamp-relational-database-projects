857f35f11a1c67651fd86fb4a96608372769b8e6
al-script <alec.sorensen.as@gmail.com>
2023-05-25 18:47:50 -0700

    Fix button text, tidy old scripts.js

--- Responsive Web Design/Portfolio/scripts.js
+++ Responsive Web Design/Portfolio/scripts.js
@@ -27,10 +27,7 @@ function toggleLightDarkMode() {
 let accordion = document.getElementsByClassName("accordion");
 let i;
 
-console.log("accordion:", accordion);
-
 for (i = 0; i < accordion.length; i++) {
-  console.log(i)
   accordion[i].addEventListener("click", function () {
     this.classList.toggle("accordion-active");
 
