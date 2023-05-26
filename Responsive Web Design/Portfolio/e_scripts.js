2c0d16959da6e0d66f5f065d14096544c1c01df2
al-script <alec.sorensen.as@gmail.com>
2023-05-25 19:16:56 -0700

    Fix light/dark/toggle scripts, update header/footer text

--- Responsive Web Design/Portfolio/e_scripts.js
+++ Responsive Web Design/Portfolio/e_scripts.js
@@ -1,40 +1,40 @@
-window.addEventListener("DOMContentLoaded", (event) => {
-  // Could add preference to local storage
-  // Handle light-mode-dark-mode toggle
-  // help from: https://stackoverflow.com/questions/56300132/how-to-override-css-prefers-color-scheme-setting
-  function toggleLightDarkMode() {
-    if (document.documentElement.classList.contains("light")) {
-      document.documentElement.classList.remove("light");
+// Could add preference to local storage
+// Handle light-mode-dark-mode toggle
+// help from: https://stackoverflow.com/questions/56300132/how-to-override-css-prefers-color-scheme-setting
+function toggleLightDarkMode() {
+  if (document.documentElement.classList.contains("light")) {
+    document.documentElement.classList.remove("light");
+    document.documentElement.classList.add("dark");
+  } else if (document.documentElement.classList.contains("dark")) {
+    document.documentElement.classList.remove("dark");
+    document.documentElement.classList.add("light");
+  } else {
+    if (
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
       document.documentElement.classList.add("dark");
-    } else if (document.documentElement.classList.contains("dark")) {
-      document.documentElement.classList.remove("dark");
+      toggleLightDarkMode();
+    } else {
       document.documentElement.classList.add("light");
-    } else {
-      if (
-        window.matchMedia &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches
-      ) {
-        document.documentElement.classList.add("dark");
-        toggleLightDarkMode();
-      } else {
-        document.documentElement.classList.add("light");
-        toggleLightDarkMode();
-      }
+      toggleLightDarkMode();
     }
   }
+}
 
-  function navMenuToggle() {
-    let navMenu = document.getElementById("nav-top-project-link-container");
-    let toggleBars = document.getElementById("nav-top-toggle");
-    if (navMenu.classList.contains("toggle-nav-top-menu")) {
-      navMenu.classList.remove("toggle-nav-top-menu");
-      toggleBars.classList.remove("toggle-nav-top-bars");
-    } else {
-      navMenu.classList.add("toggle-nav-top-menu");
-      toggleBars.classList.add("toggle-nav-top-bars");
-    }
+function navMenuToggle() {
+  let navMenu = document.getElementById("nav-top-project-link-container");
+  let toggleBars = document.getElementById("nav-top-toggle");
+  if (navMenu.classList.contains("toggle-nav-top-menu")) {
+    navMenu.classList.remove("toggle-nav-top-menu");
+    toggleBars.classList.remove("toggle-nav-top-bars");
+  } else {
+    navMenu.classList.add("toggle-nav-top-menu");
+    toggleBars.classList.add("toggle-nav-top-bars");
   }
+}
 
+window.addEventListener("DOMContentLoaded", (event) => {
   //  Handle accordion
   //  help from: https://www.w3schools.com/howto/howto_js_accordion.asp
   let accordion = document.getElementsByClassName("accordion-button");
