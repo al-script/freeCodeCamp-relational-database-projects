39eecc59a14fb644dc151c404ac9b14b38c22caf
al-script <alec.sorensen.as@gmail.com>
2023-06-20 16:43:58 -0700

    JS exploration

---
+++ Data Visualization/Lessons/3. Sol LeWitt in Vanilla JavaScript/scripts.js
@@ -0,0 +1,24 @@
+// const add = (a, b) => a + b;
+// const add = (a, b) => {
+//   return a + b
+// }
+// function add(a, b) {
+//   return a + b;
+// }
+
+// console.log(add(1, 2))
+
+// let object = {
+//   key: 'first value',
+//   'second key': 'second value',
+//   3: 'third value'
+// };
+
+// let testvar = 'key';
+
+// console.log(object.key)
+// console.log(object[testvar])
+// console.log(object['key'])
+// console.log(object['second key'])
+// console.log(object['3'])
+// console.log(object[3])
