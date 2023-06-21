9794898f060af6d3746c527cb0e0fa4b92d6f0f0
al-script <alec.sorensen.as@gmail.com>
2023-06-21 15:39:07 -0700

    Masking experimentation

--- Data Visualization/Lessons/3. Sol LeWitt in Vanilla JavaScript/scripts.js
+++ Data Visualization/Lessons/3. Sol LeWitt in Vanilla JavaScript/scripts.js
@@ -51,14 +51,46 @@ svg.setAttribute("width", width);
 svg.setAttribute("height", height);
 container.appendChild(svg);
 
+// const n = 100;
+// for (let i = 0; i < n; i++) {
+//   const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+//   rect.setAttribute("x", i*20);
+//   // rect.setAttribute("width", i/2);
+//   rect.setAttribute("width", 10);
+//   rect.setAttribute("height", height);
+//   svg.appendChild(rect);
+// }
+
+const mask = document.createElementNS("http://www.w3.org/2000/svg", "mask");
+mask.setAttribute("id", "circle-mask");
+svg.appendChild(mask);
+
+// when mask applied, are only seeing what is defined within the mask
+// black invisible
+// white visible
+// mask rect creates an inverse mask
+
+// white, allows visibility for the unmasked areas
+const maskRect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+maskRect.setAttribute("width", width);
+maskRect.setAttribute("height", height);
+maskRect.setAttribute("fill", "white");
+mask.appendChild(maskRect);
+
+//black, allows invisibility for the masked areas (in this case, everything within a circle, if area falls under circle, is invisible)
+const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+circle.setAttribute("cx", width / 2);
+circle.setAttribute("cy", height / 2);
+circle.setAttribute("r", 200);
+circle.setAttribute("fill", "black");
+mask.appendChild(circle);
+
 const n = 100;
 for (let i = 0; i < n; i++) {
   const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-  rect.setAttribute("x", i*20);
-  // rect.setAttribute("width", i/2);
-  rect.setAttribute("width", 10);
-  rect.setAttribute("height", height);
+  rect.setAttribute("y", i * 20);
+  rect.setAttribute("width", width);
+  rect.setAttribute("height", 10);
+  rect.setAttribute("mask", "url(#circle-mask");
   svg.appendChild(rect);
 }
-
-console.log(window)
