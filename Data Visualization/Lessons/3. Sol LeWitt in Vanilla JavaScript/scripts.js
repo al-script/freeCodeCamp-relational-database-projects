459d6f7da0c8511deec7fef678ef66157d17f6f8
al-script <alec.sorensen.as@gmail.com>
2023-06-21 13:15:08 -0700

    Begin Sol LeWitt scripting

--- Data Visualization/Lessons/3. Sol LeWitt in Vanilla JavaScript/scripts.js
+++ Data Visualization/Lessons/3. Sol LeWitt in Vanilla JavaScript/scripts.js
@@ -22,3 +22,43 @@
 // console.log(object['second key'])
 // console.log(object['3'])
 // console.log(object[3])
+
+// // *** if want to implicity return an object with es6 shorthand then use ()
+// let person = (a, b) => ({
+//   a: b,
+//   b: a
+// })
+// // can also use this for where property name matches variable name:
+// let person2 = (a, c) => ({
+//   a,
+//   c
+// })
+// console.log(person('test','test2'))
+// console.log(person2('test','test2'))
+
+// console.log(document.body)
+
+let container = document.getElementById("svg-container");
+// let svg = document.createElement('svg');
+// svg.classList.add('svg-component');
+// container.innerHTML += svg;
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+svg.setAttribute("width", width);
+svg.setAttribute("height", height);
+container.appendChild(svg);
+
+const n = 100;
+for (let i = 0; i < n; i++) {
+  const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+  rect.setAttribute("x", i*20);
+  // rect.setAttribute("width", i/2);
+  rect.setAttribute("width", 10);
+  rect.setAttribute("height", height);
+  svg.appendChild(rect);
+}
+
+console.log(window)
