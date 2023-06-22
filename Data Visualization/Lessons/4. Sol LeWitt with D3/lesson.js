88b44eed468a4d1d47fcfda467ceb02c836bef6a
al-script <alec.sorensen.as@gmail.com>
2023-06-22 14:05:20 -0700

    Continue lesson 4

--- Data Visualization/Lessons/4. Sol LeWitt with D3/lesson.js
+++ Data Visualization/Lessons/4. Sol LeWitt with D3/lesson.js
@@ -1,76 +1,158 @@
+// DOM manipulation framework
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@6.6/+esm";
-console.log(d3.selection);
 
 let container = document.getElementById("svg-container");
 
 const width = window.innerWidth;
 const height = window.innerHeight;
 
-const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-svg.setAttribute("width", width);
-svg.setAttribute("height", height);
-container.appendChild(svg);
+// const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+// svg.setAttribute("width", width);
+// svg.setAttribute("height", height);
+// container.appendChild(svg);
 
-const mask = document.createElementNS("http://www.w3.org/2000/svg", "mask");
-mask.setAttribute("id", "circle-mask");
-svg.appendChild(mask);
+// can now use d3 methods on svg
+const svg = d3
+  .select(container)
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height);
 
-// when mask applied, are only seeing what is defined within the mask
-// black invisible
-// white visible
-// mask rect creates an inverse mask
+// // apply inverse mask such that center (white) is visible, and rest (black) is hidden
+// for (let i = 0; i < n; i++) {
+//   const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+//   rect.setAttribute("y", i * 20);
+//   rect.setAttribute("width", width);
+//   rect.setAttribute("height", 10);
+//   rect.setAttribute("mask", "url(#circle-mask");
+//   svg.appendChild(rect);
+// }
 
-//black, allows invisibility for the masked areas (in this case, everything within a circle, if area falls under rectangle, is invisible)
-const maskRect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-maskRect.setAttribute("width", width);
-maskRect.setAttribute("height", height);
-maskRect.setAttribute("fill", "black");
-mask.appendChild(maskRect);
+// // create data
+// const n = 100;
+// const marks = [];
+// for (let i = 0; i < n; i++) {
+//   marks.push({
+//     y: i * 20,
+//     width: width,
+//     height: 10,
+//     mask: "url(#circle-mask)",
+//   });
+// }
 
-// white, allows visibility for the unmasked areas, if area falls under white is visible
-const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-circle.setAttribute("cx", width / 2);
-circle.setAttribute("cy", height / 2);
-circle.setAttribute("r", 200);
-circle.setAttribute("fill", "white");
-mask.appendChild(circle);
+// // render data
+// // select all rects (that dont exist yet) and create rect for each mark per the passed in array
+// // join data to a rect
+// // and set attributes
+// svg
+//   .selectAll("rect")
+//   .data(marks)
+//   .join("rect")
+//   .attr("y", (d) => d.y)
+//   .attr("width", (d) => d.width)
+//   .attr("height", (d) => d.height)
+//   .attr("mask", (d) => d.mask);
 
-
-const maskInverse = document.createElementNS("http://www.w3.org/2000/svg", "mask");
-maskInverse.setAttribute("id", "circle-mask-inverse");
-svg.appendChild(maskInverse);
-
-const maskRectInverse = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-maskRectInverse.setAttribute("width", width);
-maskRectInverse.setAttribute("height", height);
-maskRectInverse.setAttribute("fill", "white");
-maskInverse.appendChild(maskRectInverse);
-
-const circleInverse = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-circleInverse.setAttribute("cx", width / 2);
-circleInverse.setAttribute("cy", height / 2);
-circleInverse.setAttribute("r", 200);
-circleInverse.setAttribute("fill", "black");
-maskInverse.appendChild(circleInverse);
-
-// apply inverse mask such that center (black) is hidden, and rest (white) is visible
+// create and render data
+// attr accepts both constants and functions, func passes in the object from array to play with
 const n = 100;
-for (let i = 0; i < n; i++) {
-  const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-  rect.setAttribute("x", i*20);
-  // rect.setAttribute("width", i/2);
-  rect.setAttribute("width", 10);
-  rect.setAttribute("height", height);
-  rect.setAttribute("mask", "url(#circle-mask-inverse");
-  svg.appendChild(rect);
-}
+svg
+  .selectAll("rect")
+  .data(d3.range(n))
+  .join("rect")
+  .attr("y", (d) => d * 20)
+  .attr("width", width)
+  .attr("height", 10)
+  .attr("mask", "url(#circle-mask)");
 
-// apply inverse mask suc center (white) is visible, and rest (black) is hidden
-for (let i = 0; i < n; i++) {
-  const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-  rect.setAttribute("y", i * 20);
-  rect.setAttribute("width", width);
-  rect.setAttribute("height", 10);
-  rect.setAttribute("mask", "url(#circle-mask");
-  svg.appendChild(rect);
-}
+svg
+  .selectAll("rect")
+  .data(d3.range(n))
+  .join("rect")
+  .attr("x", (d) => d * 20)
+  .attr("width", 10)
+  .attr("height", height)
+  .attr("mask", "url(#circle-mask-inverse)");
+
+// const mask = document.createElementNS("http://www.w3.org/2000/svg", "mask");
+// mask.setAttribute("id", "circle-mask");
+// svg.appendChild(mask);
+
+const mask = svg.append("mask").attr("id", "circle-mask");
+
+// // when mask applied, are only seeing what is defined within the mask
+// // black invisible
+// // white visible
+// // mask rect creates an inverse mask
+
+// //black, allows invisibility for the masked areas (in this case, everything within a circle, if area falls under rectangle, is invisible)
+// const maskRect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+// maskRect.setAttribute("width", width);
+// maskRect.setAttribute("height", height);
+// maskRect.setAttribute("fill", "black");
+// mask.appendChild(maskRect);
+
+mask
+  .append("rect")
+  .attr("width", width)
+  .attr("height", height)
+  .attr("fill", "black");
+
+// // white, allows visibility for the unmasked areas, if area falls under white is visible
+// const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+// circle.setAttribute("cx", width / 2);
+// circle.setAttribute("cy", height / 2);
+// circle.setAttribute("r", 200);
+// circle.setAttribute("fill", "white");
+// mask.appendChild(circle);
+
+mask
+  .append("circle")
+  .attr("cx", width / 2)
+  .attr("cy", height / 2)
+  .attr("r", 200)
+  .attr("fill", "white");
+
+// const maskInverse = document.createElementNS("http://www.w3.org/2000/svg", "mask");
+// maskInverse.setAttribute("id", "circle-mask-inverse");
+// svg.appendChild(maskInverse);
+
+const maskInverse = svg.append("mask").attr("id", "circle-mask-inverse");
+
+// const maskRectInverse = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+// maskRectInverse.setAttribute("width", width);
+// maskRectInverse.setAttribute("height", height);
+// maskRectInverse.setAttribute("fill", "white");
+// maskInverse.appendChild(maskRectInverse);
+
+maskInverse
+  .append("rect")
+  .attr("width", width)
+  .attr("height", height)
+  .attr("fill", "white");
+
+// const circleInverse = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+// circleInverse.setAttribute("cx", width / 2);
+// circleInverse.setAttribute("cy", height / 2);
+// circleInverse.setAttribute("r", 200);
+// circleInverse.setAttribute("fill", "black");
+// maskInverse.appendChild(circleInverse);
+
+maskInverse
+  .append("circle")
+  .attr("cx", width / 2)
+  .attr("cy", height / 2)
+  .attr("r", 200)
+  .attr("fill", "black");
+
+// // apply inverse mask such that center (black) is hidden, and rest (white) is visible
+// const n = 100;
+// for (let i = 0; i < n; i++) {
+//   const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+//   rect.setAttribute("x", i*20);
+//   // rect.setAttribute("width", i/2);
+//   rect.setAttribute("width", 10);
+//   rect.setAttribute("height", height);
+//   rect.setAttribute("mask", "url(#circle-mask-inverse)";
+//   svg.appendChild(rect);
+// }
