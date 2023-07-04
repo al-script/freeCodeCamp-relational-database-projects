856509e280cec50ba522ef5fa813d9f5d18bca97
al-script <alec.sorensen.as@gmail.com>
2023-07-04 11:26:16 -0700

    Add modulized version of lesson 2, exercise boilerplate

---
+++ Data Visualization/Lessons/5. D3 Selections/lesson2_modulized.js
@@ -0,0 +1,24 @@
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@6.6/+esm";
+import { vizData } from "./lesson2_vizData.js";
+import { makeData } from "./lesson2_makeData.js";
+
+let container = document.getElementById("svg-container");
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+const svg = d3
+  .select(container)
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height);
+
+let t = 0;
+
+setInterval(() => {
+  const n = 10 + Math.sin(t) * 5;
+  const data = makeData(n, t);
+  svg.call(vizData, data); // svg is first element sent to the call, function setup to expect the svg element as first argument, and other arguements that pass are already setup presumably
+  // vizData(svg, data);
+  t = t + 0.005;
+}, 1000 / 120);
