94238091ed785a46ccadb5f3e9c479d8cc902e39
al-script <alec.sorensen.as@gmail.com>
2023-07-10 16:56:38 -0700

    Add rollup-based version

---
+++ Data-Visualization/Lessons/6-JS-Modules-Build-Tools/rollup-starter-example/rollup-starter-app/src/main.js
@@ -0,0 +1,5 @@
+import somethingEvenMoreElse, { something, somethingElse } from "./something.js";
+
+console.log("Hello " + something + ", " + somethingElse + ', ' + somethingEvenMoreElse + ".");
+
+console.log(d3)
---
+++ Data-Visualization/Lessons/6-JS-Modules-Build-Tools/rollup-starter-example/rollup-starter-app/src/something.js
@@ -0,0 +1,10 @@
+import { thing } from "./thing.js";
+
+export const something = "a " + thing;
+export const somethingElse = "something else";
+
+export function foo() {}
+
+let somethingEvenMoreElse = 'another else';
+
+export default somethingEvenMoreElse;
