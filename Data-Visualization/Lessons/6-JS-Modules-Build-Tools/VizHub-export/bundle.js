a3c855503b887cb8b5d5c6d813306ce23db83704
al-script <alec.sorensen.as@gmail.com>
2023-07-10 16:33:40 -0700

    Rollup experimentation

--- Data-Visualization/Lessons/6-JS-Modules-Build-Tools/VizHub-export/bundle.js
+++ Data-Visualization/Lessons/6-JS-Modules-Build-Tools/VizHub-export/bundle.js
@@ -2,6 +2,7 @@
 	'use strict';
 
 	d3.select('body').text('Hello D3 version 7');
+	console.log('test2asd');
 
 })(d3);
 //# sourceMappingURL=bundle.js.map
--- Data-Visualization/Lessons/6-JS-Modules-Build-Tools/VizHub-export/index.js
+++ Data-Visualization/Lessons/6-JS-Modules-Build-Tools/VizHub-export/index.js
@@ -1,2 +1,3 @@
 import { select } from 'd3';
 select('body').text('Hello D3 version 7');
+console.log('test2asd')
