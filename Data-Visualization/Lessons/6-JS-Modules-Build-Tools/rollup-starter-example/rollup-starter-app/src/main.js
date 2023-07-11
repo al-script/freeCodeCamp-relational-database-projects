1d48d6a649e224561d2b48c89639dbe833971b8f
al-script <alec.sorensen.as@gmail.com>
2023-07-10 17:07:31 -0700

    Fix d3 import

--- Data-Visualization/Lessons/6-JS-Modules-Build-Tools/rollup-starter-example/rollup-starter-app/src/main.js
+++ Data-Visualization/Lessons/6-JS-Modules-Build-Tools/rollup-starter-example/rollup-starter-app/src/main.js
@@ -1,5 +1,7 @@
 import somethingEvenMoreElse, { something, somethingElse } from "./something.js";
 
+import * as d3 from 'd3';
+
 console.log("Hello " + something + ", " + somethingElse + ', ' + somethingEvenMoreElse + ".");
 
 console.log(d3)
